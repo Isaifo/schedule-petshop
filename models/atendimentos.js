417f371f5cb9ffc541f2cf0d10c510ea44eea1b3
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-12-17 12:01:21 -0300

    validando erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,19 +2,46 @@ const moment = require('moment')
 const conexao = require('../infraestutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento){
-      //  const dataCriacao = moment()
-       // const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-       // const atendimentoDatado = {...atendimento, dataCriacao, data }
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 4
+       
+        const validacoes = [
+            {
+                nome: 'data',
+                valido = dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
 
-        conexao.query(sql, atendimento, (erro,resultados) =>{
-           if(erro){
-            console.log(erro);
-           } else{
-               console.log(resultados);
-           }
-        }) 
+            {
+                nome: 'cliente',
+                valido = clienteEhValido,
+                mensagem = 'Cliente deve ter pelo menos 4 caracteres'
+            }
+        ]
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros){
+            res.status(400).json(erros)
+        } else{
+
+            const atendimentoDatado = {...atendimento, dataCriacao, data }
+
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro,resultados) =>{
+               if(erro){
+                res.status(400).json(erro);
+               } else{
+                   res.status(201).json(resultados);
+               }
+            }) 
+        }
+       
     }
 }
 
